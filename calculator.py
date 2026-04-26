2f075318551c7cdd3837ab53cbb23ef14d875cdb
jakflo <jakflo@seznam.cz>
2026-04-27 00:14:02 +0200

    pythnon app a CI

---
+++ calculator.py
@@ -0,0 +1,11 @@
+"""Funkce pro scitani dvou cisel"""
+def add(a, b):
+    return a + b
+
+def main():
+    result = add(5, 3)
+    print(f"Result: {result}")
+
+
+if __name__ == "__main__":
+    main()
---
+++ test_calculator.py
@@ -0,0 +1,16 @@
+import unittest
+from calculator import add
+
+class TestAddFunction(unittest.TestCase):
+    def test_add_positive_numbers(self):
+        self.assertEqual(add(1, 2), 3)
+
+    def test_add_negative_numbers(self):
+        self.assertEqual(add(-1, -2), -3)
+
+    def test_add_mixed_numbers(self):
+        self.assertEqual(add(1, -2), -1)
+        self.assertEqual(add(-1, 2), 1)
+        
+if __name__ == '__main__':
+    unittest.main()
